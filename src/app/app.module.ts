4cb846fe0966ce1721508bf16462d040c9de39ce
zakarianya_cgi <zakaria.nya@cgi.com>
2019-02-15 14:24:09 +0100

    [FEAT] Ajoute le composant d'affichage de la liste et ajoute en dur une liste de poste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './postlist/post-list/post-list.component';
+import { PostListItemComponent } from './postlist/post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostListItemComponent,
   ],
   imports: [
     BrowserModule,
