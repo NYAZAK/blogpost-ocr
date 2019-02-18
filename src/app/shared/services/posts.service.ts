b15b8f3ecf87e4b64e8023edfcc022acbfb88a54
zakarianya_cgi <zakaria.nya@cgi.com>
2019-02-18 11:05:29 +0100

    [FEAT] Ajoute une organisation des fichiers

---
+++ src/app/shared/services/posts.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {Post} from '../models/post.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor() { }
+}
